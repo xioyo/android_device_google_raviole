0bcbb8f5eb9bfd7ee1c4975060fd723f3cc4fe78
Michael Bestas <mkbestas@lineageos.org>
2026-02-23 00:04:49 +0200

    raviole: Inherit from gs101 common.mk
    Change-Id: I66992c62c7d299fd9c4071dc9a27463c4cebd4c3

--- device-raven.mk
+++ device-raven.mk
@@ -11,7 +11,7 @@ TARGET_KERNEL_DIR ?= device/google/raviole-kernels/aosp
 TARGET_BOARD_KERNEL_HEADERS ?= $(TARGET_KERNEL_DIR)/kernel-headers
 
 # Inherit from gs101
-include device/google/gs101/device-shipping-common.mk
+include device/google/gs101/common.mk
 
 # Camera
 $(call inherit-product-if-exists, vendor/google/camera/config.mk)
