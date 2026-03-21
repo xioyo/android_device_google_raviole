ba3cf276930f6809b13dd40d2fc82c7d4728c653
xioyo <sufiyanmp07@gmail.com>
2026-03-21 11:58:23 +0000

    raviole: Bringup @Lunaris-AOSP

--- device-raven.mk
+++ device-raven.mk
@@ -14,6 +14,12 @@ TARGET_KERNEL_PLATFORM_SOURCE := google/gs-$(TARGET_LINUX_KERNEL_VERSION)
 # Inherit from gs101
 include device/google/gs101/common.mk
 
+# Camera
+$(call inherit-product-if-exists, vendor/google/camera/config.mk)
+
+# Pixel Parts
+$(call inherit-product-if-exists, packages/apps/PixelParts/device.mk)
+
 # Overlays
 PRODUCT_PACKAGES += \
     DeviceAsWebcamRaviole \
